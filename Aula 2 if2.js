ccc78f47b1224229c272191fa9e3c4c4eae92f36
Vinicius Oliveira <75057334+Skonks@users.noreply.github.com>
2021-02-19 12:35:18 -0300

    Add files via upload

---
+++ Aula 2 if2.js
@@ -0,0 +1,17 @@
+function teste(num1) {
+    if (num1 > 7)
+        console.log(num1)
+    console.log("Oi")
+}
+
+teste(5)
+teste(9)
+
+function teste2(num2) {
+    if (num2 > 7); { // não usar ; nas estruturas de controle
+        console.log(num)
+    }
+}
+
+teste2(6)
+teste2(7)
