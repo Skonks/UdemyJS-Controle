9adf8f012aa9d36d666b73fba272cd714f77aee7
Vinicius Oliveira <75057334+Skonks@users.noreply.github.com>
2021-02-23 17:15:45 -0300

    Add files via upload

---
+++ Aula 7 for1.js
@@ -0,0 +1,14 @@
+let contador = 1
+while (contador <= 10) {
+    console.log(`Contador while = ${contador}`)
+    contador++
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Contador for ${i}`)
+}
+
+const notas = [6.7, 8.8, 9.0, 10.0, 2.3]
+for (let i = 0; i < notas.length; i++) {
+    console.log(`Contador notas ${notas[i]}`)
+}
